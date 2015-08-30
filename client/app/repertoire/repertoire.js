0247a08c3900aef69e14da927cc6ea97211c6815
chriscraig <chris@cjcraig.com>
2015-08-29 22:28:27 -0600

    fixing repertoire route, hopefully

--- client/app/repertoire/repertoire.js
+++ client/app/repertoire/repertoire.js
@@ -4,8 +4,9 @@ angular.module('app')
  
 	$stateProvider.state('repertoire', { // this is a name for our route
       url: '/', // the actual url path of the route
-      templateUrl: 'app/repertoire/repertoire.html', // the template that will load
-      controller: 'RepertoireCtrl' // the name of the controller to use
+      templateUrl: 'app/repertoire/repertoire.html'
+      // , // the template that will load
+      // controller: 'RepertoireCtrl' // the name of the controller to use
     });
 
 
